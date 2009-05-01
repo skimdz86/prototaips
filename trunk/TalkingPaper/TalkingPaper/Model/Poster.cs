9b086295a5980d02a5c22402db67de63a09d8e94
yans86 <yans86@318cc572-2352-11de-9288-5f72e443383c>
2009-05-01 15:59:21 +0000

    I nostri file sono nelle cartelle: DataAccess: interfaccia + implementazione Form: i 2 form di manuel Model: user + poster + griglia + contenuto

--- trunk/TalkingPaper/TalkingPaper/Model/Poster.cs
+++ trunk/TalkingPaper/TalkingPaper/Model/Poster.cs
@@ -11,55 +11,33 @@ namespace TalkingPaper
         private String nomePoster;
         private String descrizione;
         private String username;
-        private Griglia griglia;
+        private String nomeGriglia;
 
         private int numRighe;
         private int numColonne;
 
         private List<Contenuto> contenuti;
 
-        public Poster(String nome, String desc, String username, Griglia griglia)
+        public Poster(String nome, String desc, String username, String nomeGriglia)
         {
             this.nomePoster = nome;
             this.descrizione = desc;
             this.username = username;
-            this.griglia = griglia;
-            this.numRighe = griglia.getNumRighe();
-            this.numColonne = griglia.getNumColonne();
+            this.nomeGriglia = nomeGriglia;
             contenuti = null;
         }
 
         public String getNome() { return nomePoster; }
         public String getDescrizione() { return descrizione; }
         public String getUsername() { return username; }
-        public Griglia getGriglia() { return griglia; }
+        public String getNomeGriglia() { return nomeGriglia; }
         public List<Contenuto> getContenuti() { return contenuti; }
 
         public void setNome(String nome) { this.nomePoster = nome; }
         public void setDescrizione(String desc) { this.descrizione = desc; }
         public void setUsername(String username) { this.username = username; }
-        public void setGriglia(Griglia griglia) { this.griglia = griglia; }
+        public void setNomeGriglia(String nomeGriglia) { this.nomeGriglia = nomeGriglia; }
         public void setContenuti(List<Contenuto> contenuti) { this.contenuti = contenuti; }
-
-        // x appartiene a {A,B,..} e indica la coordinata verticale
-        // y appartiene a {1,2,..} e indica la coordinata orizzontale
-        //...funziona quindi come le matrici
-        //ATTENZIONE: l'index parte da ZERO: A1 -> 0, A2 -> 1
-        public int getIndexFromCoord(String coord)
-        {
-            int index = (coord[0] - 'A') * numColonne + (coord[1] - '1');
-            return index;
-        }
-
-        // ATTENZIONE: NON L'HO ANCORA TESTATA
-        public String getCoordFromIndex(int index)
-        {
-            char x, y;
-            int offset = 'A';
-            x = (char)((int)(index / numColonne) + offset);
-            y = (char)((index % numColonne) + '1');
-            return x.ToString() + y.ToString();
-        }
 
         //se è più comodo, si può fare getContenuto(x,y)
         public Contenuto getContenutoFromIndex(int index)
@@ -77,6 +55,5 @@ namespace TalkingPaper
             contenuti[index] = null;
         }
         
-
     }
 }
