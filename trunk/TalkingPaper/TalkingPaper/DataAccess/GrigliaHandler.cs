5cea216bfd8ae4d016927089322d50ff1fa9b4a5
skimdz86 <skimdz86@318cc572-2352-11de-9288-5f72e443383c>
2009-05-03 08:58:21 +0000

    aggiunti controlli alla gestione xml, rifatte funzioni rfidconfigmanager(ma lasciate ancora quelle vecchie sotto altro nome per sicurezza). il file rfidconfig.xml dove lo metto?

--- trunk/TalkingPaper/TalkingPaper/DataAccess/GrigliaHandler.cs
+++ trunk/TalkingPaper/TalkingPaper/DataAccess/GrigliaHandler.cs
@@ -14,105 +14,141 @@ namespace TalkingPaper.DataAccess
         //crea il file
         public void CreateGriglieTaggate()
         {
-            XmlTextWriter writer = new XmlTextWriter(filepath, Encoding.UTF8);
-            writer.WriteStartDocument();
-            writer.WriteStartElement("GriglieTaggate");
-            writer.WriteEndElement();
-            writer.WriteEndDocument();
-            writer.Close();
+            try
+            {
+                XmlTextWriter writer = new XmlTextWriter(filepath, Encoding.UTF8);
+                writer.WriteStartDocument();
+                writer.WriteStartElement("GriglieTaggate");
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Close();
+            }
+            catch (XmlException e) { Console.Write(e.StackTrace); }
         }
         public bool setGriglia(Griglia gr)
         {
-            //String ID = "Generare un id per la griglia, magari casuale";//////////////////
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            XmlElement el = doc.CreateElement("Griglia");
-            
-            el.SetAttribute("Nome", gr.getNome());
-            el.SetAttribute("Righe", gr.getNumRighe().ToString());
-            el.SetAttribute("Colonne", gr.getNumColonne().ToString());
-            List<String> tempTags = gr.getListaTag();
-            for (int i = 0; i < tempTags.Count; i++) {
-                if(tempTags[i]!=null){
-                XmlElement cella = (XmlElement)doc.CreateElement("Cella");
-                cella.SetAttribute("Coordinate",gr.getCoordFromIndex(i));
-                XmlText tag = (XmlText)doc.CreateTextNode(tempTags[i]);
-                cella.AppendChild(tag);
-                el.AppendChild(cella);
+            try
+            {
+                //String ID = "Generare un id per la griglia, magari casuale";//////////////////
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                ////controllo nome gia esistente
+                XmlNodeList l = doc.GetElementsByTagName("Griglia");
+                for (int k = 0; k < l.Count; k++) {
+                    XmlElement x = (XmlElement)l[k];
+                    if (x.GetAttribute("Nome") == gr.getNome())
+                    {
+                        stream.Close();
+                        return false;
+                    }
                 }
+                XmlElement el = doc.CreateElement("Griglia");
+                el.SetAttribute("Nome", gr.getNome());
+                el.SetAttribute("Righe", gr.getNumRighe().ToString());
+                el.SetAttribute("Colonne", gr.getNumColonne().ToString());
+                List<String> tempTags = gr.getListaTag();
+                for (int i = 0; i < tempTags.Count; i++)
+                {
+                    if (tempTags[i] != null)
+                    {
+                        XmlElement cella = (XmlElement)doc.CreateElement("Cella");
+                        cella.SetAttribute("Coordinate", gr.getCoordFromIndex(i));
+                        XmlText tag = (XmlText)doc.CreateTextNode(tempTags[i]);
+                        cella.AppendChild(tag);
+                        el.AppendChild(cella);
+                    }
+                }
+                doc.DocumentElement.AppendChild(el);
+                stream.Close();
+                doc.Save(filepath);
+                return true;
             }
-            doc.DocumentElement.AppendChild(el);
-            stream.Close();
-            doc.Save(filepath);
-            return true;
+            catch (XmlException e) { return false; }
         }
         
 
         //utile?
         public void RemoveGriglia(String idGriglia, String filepath)
         {
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            XmlNodeList list = doc.GetElementsByTagName("Griglia");
-            for (int i = 0; i < list.Count; i++)
+            try
             {
-                XmlElement rm = (XmlElement)doc.GetElementsByTagName("Griglia")[i];
-                if (rm.GetAttribute("ID") == idGriglia)
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                XmlNodeList list = doc.GetElementsByTagName("Griglia");
+                for (int i = 0; i < list.Count; i++)
                 {
-                    doc.DocumentElement.RemoveChild(rm);
+                    XmlElement rm = (XmlElement)doc.GetElementsByTagName("Griglia")[i];
+                    if (rm.GetAttribute("ID") == idGriglia)
+                    {
+                        doc.DocumentElement.RemoveChild(rm);
+                    }
                 }
+                stream.Close();
+                doc.Save(filepath);
             }
-            stream.Close();
-            doc.Save(filepath);
+            catch (XmlException e) { Console.Write(e.StackTrace); }
         }
 
         public List<Griglia> getListaGriglie()
         {
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            List<Griglia> tempGr = new List<Griglia>();
-            XmlNodeList grList = doc.GetElementsByTagName("Griglia");
-            for (int i = 0; i < grList.Count; i++)
+            try
             {
-                XmlElement x = (XmlElement)doc.GetElementsByTagName("Griglia")[i];
-                Griglia gr = new Griglia("",0,0);
-                gr.setNome(x.GetAttribute("Nome"));
-                int r=Convert.ToInt32(x.GetAttribute("Righe"));
-                int c = Convert.ToInt32(x.GetAttribute("Colonne"));
-                gr.setNumRighe(r);
-                gr.setNumColonne(c);
-                tempGr.Add(gr);
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                List<Griglia> tempGr = new List<Griglia>();
+                XmlNodeList grList = doc.GetElementsByTagName("Griglia");
+                for (int i = 0; i < grList.Count; i++)
+                {
+                    XmlElement x = (XmlElement)doc.GetElementsByTagName("Griglia")[i];
+                    Griglia gr = new Griglia("", 0, 0);
+                    gr.setNome(x.GetAttribute("Nome"));
+                    int r = Convert.ToInt32(x.GetAttribute("Righe"));
+                    int c = Convert.ToInt32(x.GetAttribute("Colonne"));
+                    gr.setNumRighe(r);
+                    gr.setNumColonne(c);
+                    tempGr.Add(gr);
+                }
+                stream.Close();
+                return tempGr;
             }
-            return tempGr;
+            catch (XmlException e) { return null; }
         }
         public Griglia getGriglia(String nome)
         {
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            Griglia tempGr = new Griglia("", 0, 0);
-            XmlNodeList grList = doc.GetElementsByTagName("Griglia");
-            for (int i = 0; i < grList.Count; i++) {
-                XmlElement x=(XmlElement)doc.GetElementsByTagName("Griglia")[i];
-                if (x.GetAttribute("Nome") == nome) {
-                    tempGr.setNome(x.GetAttribute("Nome"));
-                    int r=Convert.ToInt32(x.GetAttribute("Righe"));
-                    int c=Convert.ToInt32(x.GetAttribute("Colonne"));
-                    tempGr.setNumRighe(r);
-                    tempGr.setNumColonne(c);
-                    XmlNodeList celleList=x.GetElementsByTagName("Cella");
-                    List<String> tags=new List<string>(r*c);
-                    for(int j=0;j<celleList.Count;j++){
-                        XmlElement y=(XmlElement)x.GetElementsByTagName("Cella")[j];
-                        if(y.InnerText!=null) tags[j]=y.InnerText;
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                Griglia tempGr = new Griglia("", 0, 0);
+                XmlNodeList grList = doc.GetElementsByTagName("Griglia");
+                for (int i = 0; i < grList.Count; i++)
+                {
+                    XmlElement x = (XmlElement)doc.GetElementsByTagName("Griglia")[i];
+                    if (x.GetAttribute("Nome") == nome)
+                    {
+                        tempGr.setNome(x.GetAttribute("Nome"));
+                        int r = Convert.ToInt32(x.GetAttribute("Righe"));
+                        int c = Convert.ToInt32(x.GetAttribute("Colonne"));
+                        tempGr.setNumRighe(r);
+                        tempGr.setNumColonne(c);
+                        XmlNodeList celleList = x.GetElementsByTagName("Cella");
+                        List<String> tags = new List<string>(r * c);
+                        for (int j = 0; j < celleList.Count; j++)
+                        {
+                            XmlElement y = (XmlElement)x.GetElementsByTagName("Cella")[j];
+                            if (y.InnerText != null) tags[j] = y.InnerText;//ma le coordinate nn le ripassso piu???
+                        }
+                        tempGr.setListaTag(tags);
                     }
-                    tempGr.setListaTag(tags);
                 }
+                stream.Close();
+                return tempGr;
             }
-            return tempGr;
+            catch (XmlException e) { return null; }
         }
 
     }
--- trunk/TalkingPaper/TalkingPaper/DataAccess/PosterHandler.cs
+++ trunk/TalkingPaper/TalkingPaper/DataAccess/PosterHandler.cs
@@ -12,119 +12,152 @@ namespace TalkingPaper.DataAccess
 
         public void CreateElencoPoster()
         {
-            XmlTextWriter writer = new XmlTextWriter(filepath, Encoding.UTF8);
-            writer.WriteStartDocument();
-            writer.WriteStartElement("ElencoPoster");
-            writer.WriteEndElement();
-            writer.WriteEndDocument();///////////nella guida nn c'è ma mi sa che ci va
-            writer.Close();
+            try
+            {
+                XmlTextWriter writer = new XmlTextWriter(filepath, Encoding.UTF8);
+                writer.WriteStartDocument();
+                writer.WriteStartElement("ElencoPoster");
+                writer.WriteEndElement();
+                writer.WriteEndDocument();///////////nella guida nn c'è ma mi sa che ci va
+                writer.Close();
+            }
+            catch (XmlException e) { Console.Write(e.StackTrace); }
         }
         public bool setPoster(Poster poster)
         {
-            //usiamo il nome come ID
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            XmlElement el = doc.CreateElement("Poster");
-            el.SetAttribute("idUtente", poster.getUsername());
-            el.SetAttribute("Nome", poster.getNome());
-            el.SetAttribute("idGriglia", poster.getNomeGriglia());
-            XmlElement desc = doc.CreateElement("Descrizione");
-            XmlText text = doc.CreateTextNode(poster.getDescrizione());
-            desc.AppendChild(text);
-            el.AppendChild(desc);
-            List<Contenuto> tempList = poster.getContenuti();
-            for (int i = 0; i < tempList.Count; i++) {
-                Contenuto c = (Contenuto)tempList[i];
-                XmlElement x = doc.CreateElement("Contenuto");
-                x.SetAttribute("Nome", c.getNomeContenuto());
-                x.SetAttribute("AudioPath", c.getAudioPath());
-                x.SetAttribute("VideoPath", c.getVideoPath());
-                x.SetAttribute("ImagePath", c.getImagePath());
-                x.SetAttribute("TextPath", c.getTextPath());
-                x.SetAttribute("Cella", c.getCoordinate());
-                el.AppendChild(x);
+            try
+            {
+                //usiamo il nome come ID
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                ////controllo nome gia esistente
+                XmlNodeList l = doc.GetElementsByTagName("Poster");
+                for (int k = 0; k < l.Count;k++ ) {
+                    XmlElement xel = (XmlElement)l[k];
+                    if (xel.GetAttribute("Nome") == poster.getNome())
+                    {
+                        stream.Close();
+                        return false;
+                    }
+                }
+                XmlElement el = doc.CreateElement("Poster");
+                el.SetAttribute("idUtente", poster.getUsername());
+                el.SetAttribute("Nome", poster.getNome());
+                el.SetAttribute("idGriglia", poster.getNomeGriglia());
+                XmlElement desc = doc.CreateElement("Descrizione");
+                XmlText text = doc.CreateTextNode(poster.getDescrizione());
+                desc.AppendChild(text);
+                el.AppendChild(desc);
+                List<Contenuto> tempList = poster.getContenuti();
+                for (int i = 0; i < tempList.Count; i++)
+                {
+                    Contenuto c = (Contenuto)tempList[i];
+                    XmlElement x = doc.CreateElement("Contenuto");
+                    x.SetAttribute("Nome", c.getNomeContenuto());
+                    x.SetAttribute("AudioPath", c.getAudioPath());
+                    x.SetAttribute("VideoPath", c.getVideoPath());
+                    x.SetAttribute("ImagePath", c.getImagePath());
+                    x.SetAttribute("TextPath", c.getTextPath());
+                    x.SetAttribute("Cella", c.getCoordinate());
+                    el.AppendChild(x);
+                }
+                doc.DocumentElement.AppendChild(el);
+                stream.Close();
+                doc.Save(filepath);
+                return true;
             }
-            doc.DocumentElement.AppendChild(el);
-            stream.Close();
-            doc.Save(filepath);
-            return true;
+            catch (XmlException e) { return false; }
         }
         public Poster getPoster(String nomePoster) {
 
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            Poster tempPoster = new Poster("", "", "", "");
-            XmlNodeList posList = doc.GetElementsByTagName("Poster");
-            for (int i = 0; i < posList.Count; i++)
+            try
             {
-                XmlElement x = (XmlElement)doc.GetElementsByTagName("Poster")[i];
-                if (x.GetAttribute("Nome") == nomePoster)
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                Poster tempPoster = new Poster("", "", "", "");
+                XmlNodeList posList = doc.GetElementsByTagName("Poster");
+                for (int i = 0; i < posList.Count; i++)
                 {
-                    tempPoster.setNome(x.GetAttribute("Nome"));
-                    tempPoster.setUsername(x.GetAttribute("idUtente"));
-                    tempPoster.setNomeGriglia(x.GetAttribute("idGriglia"));
-                    XmlNodeList descList = x.GetElementsByTagName("Descrizione");
-                    XmlElement desc=(XmlElement)descList[0];
-                    tempPoster.setDescrizione(desc.InnerText);
-                    XmlNodeList contList=x.GetElementsByTagName("Contenuto");
-                    List<Contenuto> tempCont=new List<Contenuto>();
-                    for (int j = 0; j < contList.Count; j++)
+                    XmlElement x = (XmlElement)doc.GetElementsByTagName("Poster")[i];
+                    if (x.GetAttribute("Nome") == nomePoster)
                     {
-                        XmlElement y = (XmlElement)x.GetElementsByTagName("Contenuto")[j];
-                        Contenuto c=new Contenuto("","","","","");
-                        c.setNomeContenuto(y.GetAttribute("Nome"));
-                        c.setAudioPath(y.GetAttribute("AudioPath"));
-                        c.setVideoPath(y.GetAttribute("VideoPath"));
-                        c.setImagePath(y.GetAttribute("ImagePath"));
-                        c.setTextPath(y.GetAttribute("TextPath"));
-                        c.setCoordinate(y.GetAttribute("Cella"));
-                        tempCont.Add(c);
+                        tempPoster.setNome(x.GetAttribute("Nome"));
+                        tempPoster.setUsername(x.GetAttribute("idUtente"));
+                        tempPoster.setNomeGriglia(x.GetAttribute("idGriglia"));
+                        XmlNodeList descList = x.GetElementsByTagName("Descrizione");
+                        XmlElement desc = (XmlElement)descList[0];
+                        tempPoster.setDescrizione(desc.InnerText);
+                        XmlNodeList contList = x.GetElementsByTagName("Contenuto");
+                        List<Contenuto> tempCont = new List<Contenuto>();
+                        for (int j = 0; j < contList.Count; j++)
+                        {
+                            XmlElement y = (XmlElement)x.GetElementsByTagName("Contenuto")[j];
+                            Contenuto c = new Contenuto("", "", "", "", "");
+                            c.setNomeContenuto(y.GetAttribute("Nome"));
+                            c.setAudioPath(y.GetAttribute("AudioPath"));
+                            c.setVideoPath(y.GetAttribute("VideoPath"));
+                            c.setImagePath(y.GetAttribute("ImagePath"));
+                            c.setTextPath(y.GetAttribute("TextPath"));
+                            c.setCoordinate(y.GetAttribute("Cella"));
+                            tempCont.Add(c);
+                        }
+                        tempPoster.setContenuti(tempCont);
                     }
-                    tempPoster.setContenuti(tempCont);
                 }
+                stream.Close();
+                return tempPoster;
             }
-            return tempPoster;
+            catch (XmlException e) { return null; }
         }
         public List<Poster> getListaPoster() {
 
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            List<Poster> tempPos = new List<Poster>();
-            XmlNodeList posList = doc.GetElementsByTagName("Poster");
-            for (int i = 0; i < posList.Count; i++)
+            try
             {
-                XmlElement x = (XmlElement)doc.GetElementsByTagName("Poster")[i];
-                Poster pos = new Poster("", "", "","");
-                pos.setNome(x.GetAttribute("Nome"));
-                pos.setUsername(x.GetAttribute("idUtente"));
-                pos.setNomeGriglia(x.GetAttribute("idGriglia"));
-                XmlNodeList descList = x.GetElementsByTagName("Descrizione");
-                XmlElement desc = (XmlElement)descList[0];
-                pos.setDescrizione(desc.InnerText);
-                tempPos.Add(pos);
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                List<Poster> tempPos = new List<Poster>();
+                XmlNodeList posList = doc.GetElementsByTagName("Poster");
+                for (int i = 0; i < posList.Count; i++)
+                {
+                    XmlElement x = (XmlElement)doc.GetElementsByTagName("Poster")[i];
+                    Poster pos = new Poster("", "", "", "");
+                    pos.setNome(x.GetAttribute("Nome"));
+                    pos.setUsername(x.GetAttribute("idUtente"));
+                    pos.setNomeGriglia(x.GetAttribute("idGriglia"));
+                    XmlNodeList descList = x.GetElementsByTagName("Descrizione");
+                    XmlElement desc = (XmlElement)descList[0];
+                    pos.setDescrizione(desc.InnerText);
+                    tempPos.Add(pos);
+                }
+                stream.Close();
+                return tempPos;
             }
-            return tempPos;
+            catch (XmlException e) { return null; }
         }
         public bool removePoster(String nomePoster) {
 
-            XmlDocument doc = new XmlDocument();
-            FileStream stream = new FileStream(filepath, FileMode.Open);
-            doc.Load(stream);
-            XmlNodeList list = doc.GetElementsByTagName("Poster");
-            for (int i = 0; i < list.Count; i++)
+            try
             {
-                XmlElement rm = (XmlElement)doc.GetElementsByTagName("Poster")[i];
-                if (rm.GetAttribute("Nome") == nomePoster)
+                XmlDocument doc = new XmlDocument();
+                FileStream stream = new FileStream(filepath, FileMode.Open);
+                doc.Load(stream);
+                XmlNodeList list = doc.GetElementsByTagName("Poster");
+                for (int i = 0; i < list.Count; i++)
                 {
-                    doc.DocumentElement.RemoveChild(rm);
+                    XmlElement rm = (XmlElement)doc.GetElementsByTagName("Poster")[i];
+                    if (rm.GetAttribute("Nome") == nomePoster)
+                    {
+                        doc.DocumentElement.RemoveChild(rm);
+                    }
                 }
+                stream.Close();
+                doc.Save(filepath);
+                return true;
             }
-            stream.Close();
-            doc.Save(filepath);
-            return true;
+            catch (XmlException e) { return false; Console.Write("Non esiste questo poster"); }
         }
         
     }
