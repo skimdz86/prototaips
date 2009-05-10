c800939e08e811807fb48b7286125ac3f717e942
skimdz86 <skimdz86@318cc572-2352-11de-9288-5f72e443383c>
2009-05-10 14:52:05 +0000

    fatto childrencontrol

--- TalkingPaper/TalkingPaper/ControlLogic/ChildrenControl.cs
+++ TalkingPaper/TalkingPaper/ControlLogic/ChildrenControl.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TalkingPaper.Common;
 
 namespace TalkingPaper.ControlLogic
 {
     class ChildrenControl
     {
+        public List<Model.Griglia> leggiGriglie()
+        {
+            return Global.dataHandler.getListaGriglie();
+        }
+
+        public Model.Griglia getGriglia(String nomeGriglia)
+        {
+            return Global.dataHandler.getGriglia(nomeGriglia);
+        }
+        public List<Model.Poster> getListaPoster()
+        {
+            return Global.dataHandler.getListaPoster();
+        }
+        public Model.Poster getPoster(String nomePoster) 
+        {
+            return Global.dataHandler.getPoster(nomePoster);
+        }
+        public bool salvaPoster(Model.Poster poster) 
+        {
+            return Global.dataHandler.setPoster(poster);
+        }
+
     }
 }
