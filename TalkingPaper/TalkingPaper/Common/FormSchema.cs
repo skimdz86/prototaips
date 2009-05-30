a3e95b018a494635318a08dcd85d492b56ad1636
skimdz86 <skimdz86@318cc572-2352-11de-9288-5f72e443383c>
2009-05-30 12:34:03 +0000

    commenti a Common

--- TalkingPaper/TalkingPaper/Common/FormSchema.cs
+++ TalkingPaper/TalkingPaper/Common/FormSchema.cs
@@ -15,15 +15,11 @@ namespace TalkingPaper.Common
         public FormSchema()
         {
             InitializeComponent();
+            /*Comandi per il fullscreen*/
             //this.WindowState = FormWindowState.Maximized;
             //this.FormBorderStyle = FormBorderStyle.None;
             //this.TopMost = true;
         }
 
-        /*private void help_Click(object sender, EventArgs e)
-        {
-            HelpFormSchema helpForm = new HelpFormSchema("Per questa pagina non è disponibile l'aiuto.");
-            NavigationControl.showDialog(helpForm);
-        }*/
     }
 }
